bd65a1752679906b49e63a4f9ab64562d5f3e815
calobetoleon <cleon@fuxion.net>
2023-07-26 10:59:58 -0500

    change in calling d3 library

--- src/js/load-data.js
+++ src/js/load-data.js
@@ -1,9 +1,9 @@
 import { drawPeruMap } from "./perumap.js"
 import data from '../data/entel_movil_parsed.json'
-import * as d3 from "d3"
+import { json } from "d3-fetch"
 
 // import the cartography and pass it with data
-d3.json("https://cdn.jsdelivr.net/npm/pe-atlas@1.0.1/districts-100k.json")
+json("https://cdn.jsdelivr.net/npm/pe-atlas@1.0.1/districts-100k.json")
     .then(response => {
         drawPeruMap(data, response)
     })
