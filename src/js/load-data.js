f5ce69cbd3d3ed9574956900f517502e6cd66b22
calobetoleon <cleon@fuxion.net>
2023-07-26 15:51:56 -0500

    Updates

--- src/js/load-data.js
+++
@@ -1,9 +0,0 @@
-import { drawPeruMap } from "./perumap.js"
-import data from '../data/entel_movil_parsed.json'
-import { json } from "d3-fetch"
-
-// import the cartography and pass it with data
-json("https://cdn.jsdelivr.net/npm/pe-atlas@1.0.1/districts-100k.json")
-    .then(response => {
-        drawPeruMap(data, response)
-    })
